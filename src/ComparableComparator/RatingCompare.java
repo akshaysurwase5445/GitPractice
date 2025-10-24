b9b3c0bfa412d40e145efa324b2a7b5ec85d52f5
Akshay <akshay5445surwase@gmail.com>
2025-10-24 19:35:03 +0530

    This commit will add new comparable comparator funtionality to codebase

---
+++ src/ComparableComparator/RatingCompare.java
@@ -0,0 +1,10 @@
+package ComparableComparator;
+
+import java.util.Comparator;
+
+public class RatingCompare implements Comparator<Movie> {
+    @Override
+    public int compare(Movie o1, Movie o2) {
+        return 0;
+    }
+}
